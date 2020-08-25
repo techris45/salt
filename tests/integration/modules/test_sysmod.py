ed0b009d065fc8a0cdaccd17e18dacb29e6ee5a0
Pedro Algarvio <pedro@algarvio.me>
2020-08-25 16:17:24 +0100

    Windows just needs a litle more time...

--- tests/integration/modules/test_sysmod.py
+++ tests/integration/modules/test_sysmod.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, print_function, unicode_literals
-
 import pytest
-from salt.ext import six
+import salt.utils.platform
 from tests.support.case import ModuleCase
 from tests.support.helpers import slowTest
 
@@ -19,17 +15,20 @@ class SysModuleTest(ModuleCase):
         """
         Make sure no functions are exposed that don't have valid docstrings
         """
-        ret = self.run_function("runtests_helpers.get_invalid_docs")
+        timeout = 300
+        if salt.utils.platform.is_windows():
+            timeout = 600
+        ret = self.run_function("runtests_helpers.get_invalid_docs", timeout=timeout)
         if ret == {"missing_docstring": [], "missing_cli_example": []}:
             return
 
-        if isinstance(ret, six.string_types):
+        if isinstance(ret, str):
             self.fail(ret)
 
         self.fail(
             "There are some functions which do not have a docstring or do not "
-            "have an example:\nNo docstring:\n{0}\nNo example:\n{1}\n".format(
-                "\n".join(["  - {0}".format(f) for f in ret["missing_docstring"]]),
-                "\n".join(["  - {0}".format(f) for f in ret["missing_cli_example"]]),
+            "have an example:\nNo docstring:\n{}\nNo example:\n{}\n".format(
+                "\n".join(["  - {}".format(f) for f in ret["missing_docstring"]]),
+                "\n".join(["  - {}".format(f) for f in ret["missing_cli_example"]]),
             )
         )
